15e858730f760b54dfa30955e9f7d5223aea5705
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-01-27 04:27:37 +0700

    refactor the code to test both arduino and esp32 boards

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,11 +1,16 @@
-// A simple Sketch for testing out the Arduino. No IO pin connection is needed.
+// A simple Sketch for testing out the Arduino board or the ESP32 board. No IO pin connection is needed.
+
+// Config the ledPin acording to the testing board
+// const int ledPin = 4; // The flashlight on the ESP32-CAM board (GPIO 4)
+const int ledPin = LED_BUILTIN; // The built-in led on the Arduino board (GPIO 13)
+
 void setup(){
-    pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
+    pinMode(ledPin, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
 }
 
 void loop(){
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    digitalWrite(ledPin, HIGH); // turn the LED on (HIGH is the voltage level)
     delay(1000);                     // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    digitalWrite(ledPin, LOW);  // turn the LED off by making the voltage LOW
     delay(1000);                     // wait for a second
 }
