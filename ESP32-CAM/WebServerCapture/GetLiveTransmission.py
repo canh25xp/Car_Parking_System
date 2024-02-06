17bc0f4ce4aac16d908cbb3e10164efa6bf7c1fa
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-06 15:53:18 +0700

    rename param

--- ESP32-CAM/WebServerCapture/GetLiveTransmission.py
+++ ESP32-CAM/WebServerCapture/GetLiveTransmission.py
@@ -5,14 +5,14 @@ from concurrent.futures import ProcessPoolExecutor
 
 url = 'http://192.168.137.100/cam-hi.jpg'
 
-def run1():
+def run():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
     while True:
         img_resp = urllib.request.urlopen(url)
         imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-        im = cv2.imdecode(imgnp, -1)
+        img = cv2.imdecode(imgnp, cv2.IMREAD_COLOR)
 
-        cv2.imshow('live transmission', im)
+        cv2.imshow('live transmission', img)
         key = cv2.waitKey(5)
         if key == ord('q'):
             break
@@ -23,4 +23,4 @@ def run1():
 if __name__ == '__main__':
     print("Live Stream Started")
     with ProcessPoolExecutor() as executer:
-            f1=executer.submit(run1)
+            f1=executer.submit(run)
