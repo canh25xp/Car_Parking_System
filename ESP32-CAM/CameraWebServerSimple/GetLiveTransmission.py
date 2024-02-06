65c524accead44f0585f26d2963b63d3e9799231
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-07 02:50:44 +0700

    remove multithreading

--- ESP32-CAM/CameraWebServerSimple/GetLiveTransmission.py
+++ ESP32-CAM/CameraWebServerSimple/GetLiveTransmission.py
@@ -1,9 +1,8 @@
 import cv2
 from urllib.request import urlopen
 import numpy as np
-from concurrent.futures import ProcessPoolExecutor
 
-url = 'http://192.168.137.100/cam-hi.jpg'
+url = 'http://192.168.137.128/cam-hi.jpg'
 
 def run():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
@@ -20,7 +19,17 @@ def run():
     cv2.destroyAllWindows()
 
 
+def getStillImage():
+    img_resp = urlopen(url)
+    imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+    img = cv2.imdecode(imgnp, cv2.IMREAD_COLOR)
+
+    cv2.imshow('live transmission', img)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()  
+
+
 if __name__ == '__main__':
     print("Live Stream Started")
-    with ProcessPoolExecutor() as executer:
-            f1=executer.submit(run)
+    run()
