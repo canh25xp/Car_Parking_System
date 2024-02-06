65746ea36aae94def081e2dbfaa3b2978dccfe6f
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-06 15:47:41 +0700

    format code

--- ESP32-CAM/WebServerCapture/GetLiveTransmission.py
+++ ESP32-CAM/WebServerCapture/GetLiveTransmission.py
@@ -3,19 +3,18 @@ import urllib.request
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
 
-url='http://192.168.137.30/cam-hi.jpg'
-im=None
+url = 'http://192.168.137.100/cam-hi.jpg'
 
 def run1():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
     while True:
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        im = cv2.imdecode(imgnp,-1)
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        im = cv2.imdecode(imgnp, -1)
 
-        cv2.imshow('live transmission',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
+        cv2.imshow('live transmission', im)
+        key = cv2.waitKey(5)
+        if key == ord('q'):
             break
 
     cv2.destroyAllWindows()
