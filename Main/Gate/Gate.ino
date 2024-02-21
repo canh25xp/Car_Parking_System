4c29813d13e762ce316a7f0a252eb52c308f54d8
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-21 12:13:41 +0700

    get uid method

--- Main/Gate/Gate.ino
+++ Main/Gate/Gate.ino
@@ -1,14 +1,18 @@
+#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
 
 
-// set the LCD number of columns and rows
+// Set the LCD number of columns and rows
 #define LCD_COLS        16
 #define LCD_ROWS        2
 
-// Pins definition
+// Pins connections
+#define LCD_SDA         SDA     // D2
+#define LCD_SCL         SCL     // D1
+
 #define RFID_RST_PIN    D4
 #define RFID_SDA_PIN    SS      // D8
 #define RFID_SCK_PIN    SCK     // D5
@@ -32,7 +36,7 @@ void setup() {
 
     // Initialize servo
     servo.attach(SERVO_PIN, 500, 2400); // The nodeMCU need to config the min and max value
-    ServoTest();
+    Servo_Test();
 
     // Initialize LCD
     lcd.init();
@@ -50,35 +54,56 @@ void setup() {
 }
 
 void loop() {
-    // set cursor to first column, first row
+    // Look for new cards
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+        return;
+    }
+    // Select one of the cards
+    if (!mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+
+    String UID = RFID_GetUID();
+    lcd.clear();
     lcd.setCursor(0, 0);
-    // print message
-    lcd.print("Close");
-    ServoClose();
-    servo.write(0);
-    delay(1000);
-    // clears the display to print new message
-    lcd.clear();
-    // set cursor to first column, second row
-    lcd.setCursor(0, 1);
-    lcd.print("Open");
-    ServoOpen();
-    servo.write(90);
-    delay(1000);
-    lcd.clear();
+    lcd.print(UID);
+    delay(3000);
+
+
+    // lcd.setCursor(0, 0); // set cursor to first column, first row
+    // lcd.print("Close");
+    // Servo_Close();
+    // delay(1000);
+    // lcd.clear();
+
+    // lcd.setCursor(0, 1); // set cursor to first column, second row
+    // lcd.print("Open");
+    // Servo_Open();
+    // delay(1000);
+    // lcd.clear();
 }
 
-void ServoTest() {
+void Servo_Test() {
     for (int i = 0; i <= 180; i++) {
         servo.write(i);
         delay(10);
     }
 }
 
-void ServoClose() {
+void Servo_Close() {
     servo.write(0);
 }
 
-void ServoOpen() {
+void Servo_Open() {
     servo.write(90);
+}
+
+String RFID_GetUID() {
+    String content = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+        content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+    content.toUpperCase();
+    return content.substring(1);
 }
