c46ab2ae44d4bf74fd78370167e9e7be5681aa03
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-21 12:12:02 +0700

    remove redundancy

--- Test/RFID_CheckUID/RFID-CheckUID.ino
+++ Test/RFID_CheckUID/RFID-CheckUID.ino
@@ -30,7 +30,6 @@ void loop() {
     //Show UID on serial monitor
     Serial.print("UID tag :");
     String content = "";
-    byte letter;
     for (byte i = 0; i < mfrc522.uid.size; i++) {
         Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
         Serial.print(mfrc522.uid.uidByte[i], HEX);
