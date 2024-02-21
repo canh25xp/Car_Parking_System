57b81a29697f0d1e0173e037d41e0b57386474ab
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-21 10:42:08 +0700

    test lcd and servo

--- Main/Gate/Gate.ino
+++ Main/Gate/Gate.ino
@@ -5,38 +5,48 @@
 
 
 // set the LCD number of columns and rows
-#define LCDCOLUMNS 16
-#define LCDROWS 2
+#define LCD_COLS        16
+#define LCD_ROWS        2
 
 // Pins definition
-#define RST_PIN         D4
-#define SS_PIN          D8
+#define RFID_RST_PIN    D4
+#define RFID_SDA_PIN    SS      // D8
+#define RFID_SCK_PIN    SCK     // D5
+#define RFID_MOSI_PIN   MOSI    // D7
+#define RFID_MISO_PIN   MISO    // D6
+
 #define SERVO_PIN       D3
 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, LCDCOLUMNS, LCDROWS);
+// Create LCD instance
+LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+// Create MFRC522 instance.
+MFRC522 mfrc522(RFID_SDA_PIN, RFID_RST_PIN);
 
-Servo barrie;  // create servo object to control a servo
+// Create Servo instance
+Servo servo;
 
 void setup() {
-    Serial.begin(9600);   // Initiate a serial communication
+    // Initialize Serial Communication
+    Serial.begin(9600);
 
-    barrie.attach(SERVO_PIN);  // attaches the servo on GIO2 to the servo object
+    // Initialize servo
+    servo.attach(SERVO_PIN, 500, 2400); // The nodeMCU need to config the min and max value
+    ServoTest();
 
-    // initialize LCD
+    // Initialize LCD
     lcd.init();
-    lcd.backlight(); // turn on LCD backlight
+    lcd.backlight();
+
+    // Initialize SPI bus
+    SPI.begin();
+
+    // Initialize MFRC522 RFID
+    mfrc522.PCD_Init();
 
 
-    SPI.begin();      // Initiate  SPI bus
-    mfrc522.PCD_Init();   // Initiate MFRC522
-    Serial.println("Approximate your card to the reader...");
     Serial.println();
-
-    TestServo(barrie);
+    Serial.println("Approximate your card to the reader...");
 }
 
 void loop() {
@@ -44,29 +54,31 @@ void loop() {
     lcd.setCursor(0, 0);
     // print message
     lcd.print("Close");
-    Close(barrie);
+    ServoClose();
+    servo.write(0);
     delay(1000);
     // clears the display to print new message
     lcd.clear();
     // set cursor to first column, second row
     lcd.setCursor(0, 1);
     lcd.print("Open");
-    Open(barrie);
+    ServoOpen();
+    servo.write(90);
     delay(1000);
     lcd.clear();
 }
 
-void TestServo(Servo _servo) {
+void ServoTest() {
     for (int i = 0; i <= 180; i++) {
-        _servo.write(i);
+        servo.write(i);
         delay(10);
     }
 }
 
-void Close(Servo _servo) {
-    _servo.write(0);
+void ServoClose() {
+    servo.write(0);
 }
 
-void Open(Servo _servo) {
-    _servo.write(90);
+void ServoOpen() {
+    servo.write(90);
 }
