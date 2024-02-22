2be3c3bda81a9aec035a255084b25a68768d5ca5
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-23 00:06:04 +0700

    .

--- Test/ESP8266_AsyncWebSocketServo/ESP8266_AsyncWebSocketServo.ino
+++ Test/ESP8266_AsyncWebSocketServo/ESP8266_AsyncWebSocketServo.ino
@@ -165,53 +165,6 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 
 
-void handleWebSocketMessage(void* arg, uint8_t* data, size_t len) {
-    AwsFrameInfo* info = (AwsFrameInfo*) arg;
-    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-        data[len] = 0;
-        if (strcmp((char*) data, "toggle") == 0) {
-            STATE = !STATE;
-            ws.textAll(String(STATE));
-        }
-    }
-}
-
-void onEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type,
-             void* arg, uint8_t* data, size_t len) {
-    switch (type) {
-        case WS_EVT_CONNECT:
-            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-            break;
-        case WS_EVT_DISCONNECT:
-            Serial.printf("WebSocket client #%u disconnected\n", client->id());
-            break;
-        case WS_EVT_DATA:
-            handleWebSocketMessage(arg, data, len);
-            break;
-        case WS_EVT_PONG:
-        case WS_EVT_ERROR:
-            break;
-    }
-}
-
-void initWebSocket() {
-    ws.onEvent(onEvent);
-    server.addHandler(&ws);
-}
-
-String processor(const String& var) {
-    Serial.println(var);
-    if (var == "STATE") {
-        if (STATE) {
-            return "OPEN";
-        }
-        else {
-            return "CLOSE";
-        }
-    }
-    return String();
-}
-
 void setup() {
     // Serial port for debugging purposes
     Serial.begin(115200);
@@ -249,4 +202,53 @@ void loop() {
     else {
         barrier.Close();
     }
+}
+
+
+void handleWebSocketMessage(void* arg, uint8_t* data, size_t len) {
+    AwsFrameInfo* info = (AwsFrameInfo*) arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+        data[len] = 0;
+        if (strcmp((char*) data, "toggle") == 0) {
+            STATE = !STATE;
+            ws.textAll(String(STATE));
+        }
+    }
+}
+
+
+void onEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type,
+             void* arg, uint8_t* data, size_t len) {
+    switch (type) {
+        case WS_EVT_CONNECT:
+            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+            break;
+        case WS_EVT_DISCONNECT:
+            Serial.printf("WebSocket client #%u disconnected\n", client->id());
+            break;
+        case WS_EVT_DATA:
+            handleWebSocketMessage(arg, data, len);
+            break;
+        case WS_EVT_PONG:
+        case WS_EVT_ERROR:
+            break;
+    }
+}
+
+void initWebSocket() {
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
+
+String processor(const String& var) {
+    Serial.println(var);
+    if (var == "STATE") {
+        if (STATE) {
+            return "OPEN";
+        }
+        else {
+            return "CLOSE";
+        }
+    }
+    return String();
 }
