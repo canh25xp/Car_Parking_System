b5f69580e3f02a8cc330b834732132ae0d9144b2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-23 22:47:50 +0700

    .

--- Main/Gate/RFID.h
+++ Main/Gate/RFID.h
@@ -1,11 +1,3 @@
 #include <MFRC522.h>
 
-String RFID_GetUID(const MFRC522& rfid) {
-    String content = "";
-    for (byte i = 0; i < rfid.uid.size; i++) {
-        content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
-        content.concat(String(rfid.uid.uidByte[i], HEX));
-    }
-    content.toUpperCase();
-    return content.substring(1);
-}
+String RFID_GetUID(const MFRC522& rfid);
