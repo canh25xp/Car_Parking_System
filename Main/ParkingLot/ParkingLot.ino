3592669fc03c811091d1f5de6b8282683aadc2e7
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-02-23 20:18:38 +0700

    .

--- Main/ParkingLot/ParkingLot.ino
+++ Main/ParkingLot/ParkingLot.ino
@@ -16,16 +16,12 @@
 
 // Wifi credential
 const char* SSID = "ESP8266_NodeMCU";
-const char* PSWD = "12345678";
+const char* PSWD = "123456788";
 
 // Create ultrasonic sensor instances
 NewPing sonar1(TRIG1, ECHO1, MAX_DISTANCE);
 NewPing sonar2(TRIG2, ECHO2, MAX_DISTANCE);
 NewPing sonar3(TRIG3, ECHO3, MAX_DISTANCE);
-
-int val1 = 0;
-int val2 = 0;
-int val3 = 0;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -54,10 +50,13 @@ void setup() {
         request->send_P(200, "text/plain", String(sonar3.ping_cm()).c_str());
         });
     server.on("/status", HTTP_GET, [] (AsyncWebServerRequest* request) {
-        request->send_P(200, "text/plain", GetParkingLotStatus().c_str());
+        request->send_P(200, "text/plain", GetEmptyLot().c_str());
         });
-
     server.on("/count", HTTP_GET, [] (AsyncWebServerRequest* request) {
+        request->send_P(200, "text/plain", String(CountEmptyLot()).c_str());
+        });
+    
+    server.on("/stationNum", HTTP_GET, [] (AsyncWebServerRequest* request) {
         request->send_P(200, "text/plain", String(WiFi.softAPgetStationNum()).c_str());
         });
 
@@ -66,21 +65,27 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(GetParkingLotStatus());
-    delay(1000);
+    // Serial.println(GetEmptyLot());
+    // delay(1000);
 }
 
-String GetParkingLotStatus() {
-    UpdateVal();
-    return String(IsEmptyLot(val1)) + " " + String(IsEmptyLot(val2)) + " " + String(IsEmptyLot(val3));
+String GetEmptyLot() {
+    auto _val1 = sonar1.ping_cm();
+    auto _val2 = sonar2.ping_cm();
+    auto _val3 = sonar3.ping_cm();
+    return String(IsEmptyLot(_val1)) + " " + String(IsEmptyLot(_val2)) + " " + String(IsEmptyLot(_val3));
 }
 
-void UpdateVal() {
-    val1 = sonar1.ping_cm();
-    val2 = sonar2.ping_cm();
-    val3 = sonar3.ping_cm();
+int CountEmptyLot() {
+    auto _val1 = sonar1.ping_cm();
+    auto _val2 = sonar2.ping_cm();
+    auto _val3 = sonar3.ping_cm();
+
+    int count = IsEmptyLot(_val1) + IsEmptyLot(_val2) + IsEmptyLot(_val3);
+    return count;
 }
+
 
 bool IsEmptyLot(int _distance) {
-    return (_distance > 0 && _distance < THRESHOLH) ? true : false;
+    return (_distance > 0 && _distance < THRESHOLH) ? false : true;
 }
